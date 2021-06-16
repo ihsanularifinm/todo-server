e537cdc443ea025a05c4928dac9ce38166cd2a1c
zsihsan <72428799+zsihsan@users.noreply.github.com>
2021-06-16 10:50:05 +0700

    Tahap 4 - point 5-9

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express'),
     cors = require('cors'),
     app = express(),
-    db = require('./connection/db.js');
+    db = require('./connection/db.js'),
+    auth = require('./middleware/auth.js');
 
 app.use(express.json());
 app.use(cors());
@@ -15,19 +16,7 @@ var server = {
     port: 3080,
 };
 
-app.get('/', (req, res) => {
-    res.send(`
-    <html>
-        <body>
-            <form action="/todo" method="post">
-            <input name="deskripsi" />
-            <button> Add </button>
-            </form>
-        </body>
-    </html>`);
-});
-
-app.get('/todo', (req, res) => {
+app.get('/todo', auth, (req, res) => {
     let sql = `SELECT * FROM todolist`;
     db.query(sql, (err, data) => {
         if (err) throw err;
@@ -35,16 +24,16 @@ app.get('/todo', (req, res) => {
     });
 });
 
-app.post('/todo', (req, res) => {
+app.post('/todo', auth, (req, res) => {
     let sql = `INSERT INTO todolist(deskripsi) VALUES (?)`;
     let values = [req.body.deskripsi];
-    db.query(sql, [values], function (err) {
-        if (err) throw err;
-        res.send(res.insertId);
+    db.query(sql, values, (error, results, fields) => {
+        if (error) throw error;
+        res.json({ id: results.insertId });
     });
 });
 
-app.delete('/todo/:id', function (req, res) {
+app.delete('/todo/:id', auth, function (req, res) {
     console.log(req.params.id);
     let sql = `DELETE FROM todolist WHERE id=(?)`;
     let values = [req.params.id];
@@ -54,18 +43,32 @@ app.delete('/todo/:id', function (req, res) {
     });
 });
 
-app.post('/user', (req, res) => {
-    let sql = `INSERT INTO users(username,password) VALUES (?)`;
-    let values = [req.body.username, req.body.password];
-    if (req.body.username.length && req.body.password.length === 0)
-        res.end(500);
-    db.query(sql, [values], (error, results, fields) => {
-        if (error) throw error;
-        res.json({ id: results.insertId });
-    });
-});
+app.post(
+    '/user',
+    (req, res, next) => {
+        let sql = `SELECT COUNT(*) as jumlah_user FROM users`;
+        db.query(sql, (error, results, fields) => {
+            if (error) throw error;
+            if (results[0].jumlah_user > 0) {
+                auth(req, res, next);
+            } else {
+                next();
+            }
+        });
+    },
+    (req, res) => {
+        let sql = `INSERT INTO users(username,password) VALUES (?)`;
+        let values = [req.body.username, req.body.password];
+        if (req.body.username.length && req.body.password.length === 0)
+            res.end(500);
+        db.query(sql, [values], (error, results, fields) => {
+            if (error) throw error;
+            res.json({ id: results.insertId });
+        });
+    }
+);
 
-app.get('/user', (req, res) => {
+app.get('/user', auth, (req, res) => {
     let sql = `SELECT * FROM users`;
     db.query(sql, (error, results, fields) => {
         if (error) throw error;
@@ -73,15 +76,30 @@ app.get('/user', (req, res) => {
     });
 });
 
-app.delete('/user/:id', (req, res) => {
-    let sql = `DELETE FROM users WHERE id=(?)`;
-    let values = [req.params.id];
+app.delete(
+    '/user/:id',
+    auth,
+    (req, res, next) => {
+        let sql = `SELECT COUNT(*) as jumlah_user FROM users`;
+        db.query(sql, (error, results, fields) => {
+            if (error) throw error;
+            if (results[0].jumlah_user <= 1) {
+                res.sendStatus(403);
+            } else {
+                next();
+            }
+        });
+    },
+    (req, res) => {
+        let sql = `DELETE FROM users WHERE id=(?)`;
+        let values = [req.params.id];
 
-    db.query(sql, values, (error, results, fields) => {
-        if (error) throw error;
-        res.json({ pesan: 'terhapus' });
-    });
-});
+        db.query(sql, values, (error, results, fields) => {
+            if (error) throw error;
+            res.json({ pesan: 'terhapus' });
+        });
+    }
+);
 
 app.listen(server.port, () =>
     console.log(`Server started, listening port: ${server.port}`)
