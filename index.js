85dd160e4504cb80b7ff3213af7a3216d5e889a3
zsihsan <72428799+zsihsan@users.noreply.github.com>
2021-06-16 10:24:35 +0700

    tahap 4 - menyelesaikan post get delete user

--- index.js
+++ index.js
@@ -54,6 +54,35 @@ app.delete('/todo/:id', function (req, res) {
     });
 });
 
+app.post('/user', (req, res) => {
+    let sql = `INSERT INTO users(username,password) VALUES (?)`;
+    let values = [req.body.username, req.body.password];
+    if (req.body.username.length && req.body.password.length === 0)
+        res.end(500);
+    db.query(sql, [values], (error, results, fields) => {
+        if (error) throw error;
+        res.json({ id: results.insertId });
+    });
+});
+
+app.get('/user', (req, res) => {
+    let sql = `SELECT * FROM users`;
+    db.query(sql, (error, results, fields) => {
+        if (error) throw error;
+        res.send(results);
+    });
+});
+
+app.delete('/user/:id', (req, res) => {
+    let sql = `DELETE FROM users WHERE id=(?)`;
+    let values = [req.params.id];
+
+    db.query(sql, values, (error, results, fields) => {
+        if (error) throw error;
+        res.json({ pesan: 'terhapus' });
+    });
+});
+
 app.listen(server.port, () =>
     console.log(`Server started, listening port: ${server.port}`)
 );
