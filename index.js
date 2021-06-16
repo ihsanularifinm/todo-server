1e2b56d554b58466a361ed7519a9702860292458
zsihsan <72428799+zsihsan@users.noreply.github.com>
2021-06-16 10:10:36 +0700

    tahap 4 - memisahkan file koneksi

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require('express'),
-mysql = require('mysql'),
-cors = require('cors'),
-app = express()
+    cors = require('cors'),
+    app = express(),
+    db = require('./connection/db.js');
 
-app.use(express.json())
-app.use(cors())
-app.use(express.urlencoded({
-    extended: true
-  }));
-
-db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'todo'
-})
+app.use(express.json());
+app.use(cors());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 
 var server = {
-    port : 3080
-}
+    port: 3080,
+};
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send(`
     <html>
         <body>
@@ -29,38 +24,36 @@ app.get('/', (req,res) => {
             <button> Add </button>
             </form>
         </body>
-    </html>`)
-})
+    </html>`);
+});
 
-app.get('/todo', (req,res) => {
-    let sql = `SELECT * FROM todolist`
-    db.query(sql, (err,data) => {
-        if (err) throw err
+app.get('/todo', (req, res) => {
+    let sql = `SELECT * FROM todolist`;
+    db.query(sql, (err, data) => {
+        if (err) throw err;
         res.send(data);
-    })
-})
+    });
+});
 
 app.post('/todo', (req, res) => {
-     let sql = `INSERT INTO todolist(deskripsi) VALUES (?)`
-     let values = [
-         req.body.deskripsi
-     ]
-     db.query(sql, [values], function(err) {
-        if (err) throw err;
-        res.send(res.insertId)
-      })
-})
-
-app.delete('/todo/:id', function(req,res){
-    console.log(req.params.id)
-    let sql = `DELETE FROM todolist WHERE id=(?)`
-    let values = [
-        req.params.id
-    ]
+    let sql = `INSERT INTO todolist(deskripsi) VALUES (?)`;
+    let values = [req.body.deskripsi];
     db.query(sql, [values], function (err) {
-        if(err) throw err;
-        res.end()
-    })
-})
+        if (err) throw err;
+        res.send(res.insertId);
+    });
+});
 
-app.listen(server.port, () => console.log(`Server started, listening port: ${server.port}`))
+app.delete('/todo/:id', function (req, res) {
+    console.log(req.params.id);
+    let sql = `DELETE FROM todolist WHERE id=(?)`;
+    let values = [req.params.id];
+    db.query(sql, [values], function (err) {
+        if (err) throw err;
+        res.end();
+    });
+});
+
+app.listen(server.port, () =>
+    console.log(`Server started, listening port: ${server.port}`)
+);
